ea5faee34daef8b38e53059941a181c11845bcb8
John Paul <johnpaulrayco1@gmail.com>
2025-03-30 00:55:36 +0800

    first commit for this proj

---
+++ SNAKEANDLADDER-MIDTERM/styling/Gradient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Spectre.Console;
+
+namespace SNAKEANDLADDER_MIDTERM.styling
+{
+    internal class Gradient
+    {
+        public Color startColor, endColor;
+
+        public Gradient(Color col1, Color col2)
+        {
+            startColor = col1;
+            endColor = col2;
+        }
+
+        private static Color[] GenerateGradient(Color start, Color end, int steps)
+        {
+            Color[] gradient = new Color[steps];
+
+            for (int i = 0; i < steps; i++)
+            {
+                float ratio = (float)i / (steps - 1);
+                byte r = (byte)(start.R + (end.R - start.R) * ratio);
+                byte g = (byte)(start.G + (end.G - start.G) * ratio);
+                byte b = (byte)(start.B + (end.B - start.B) * ratio);
+
+                gradient[i] = new Color(r, g, b);
+            }
+
+            return gradient;
+        }
+
+        public string GetGradientText(string text)
+        {
+            Color[] gradient = GenerateGradient(startColor, endColor, text.Length);
+            System.Text.StringBuilder result = new System.Text.StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                string hexColor = $"#{gradient[i].R:X2}{gradient[i].G:X2}{gradient[i].B:X2}"; // Convert to #RRGGBB
+                result.Append($"[bold {hexColor}]{text[i]}[/]");
+            }
+
+            return result.ToString();
+        }
+    }
+}

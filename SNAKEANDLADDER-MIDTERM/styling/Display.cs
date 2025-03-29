ae4557f9adc70368c01a34da22c418075e4613ba
John Paul <johnpaulrayco1@gmail.com>
2025-03-30 00:55:36 +0800

    first commit for this proj

---
+++ SNAKEANDLADDER-MIDTERM/styling/Display.cs
@@ -0,0 +1,32 @@
+﻿using Spectre.Console;
+using System.Text.RegularExpressions;
+
+namespace SNAKEANDLADDER_MIDTERM.styling
+{
+    internal class Display
+    {
+        public void paddingTop()
+        {
+            int paddingTop = Console.WindowHeight / 8;
+            for (int i = 0; i < paddingTop; i++)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        public void centerSpace(string text)
+        {
+            int screenWidth = Console.WindowWidth;
+
+            string strippedText = Regex.Replace(text, @"\[(.*?)\]", "");
+            int textWidth = strippedText.Length;
+
+            int space = (screenWidth - textWidth) / 2;
+            if (space < 0) space = 0;
+
+            AnsiConsole.Markup(new string(' ', space) + text + "\n");
+        }
+
+    }
+}
+
---
+++ SNAKEANDLADDER-MIDTERM/styling/PrintFormat.cs
@@ -0,0 +1,43 @@
+﻿using Spectre.Console;
+using System;
+
+namespace SNAKEANDLADDER_MIDTERM.styling
+{
+    internal class PrintFormat
+    {
+        public void printCenter(string text)
+        {
+            Display display = new Display();
+            Gradient gradient = new Gradient(Color.Yellow, Color.Orange3);
+            display.centerSpace(gradient.GetGradientText(text));
+        }
+
+        public void printCenterGreen(string text)
+        {
+            Display display = new Display();
+            Gradient gradient = new Gradient(Color.GreenYellow, Color.Blue);
+            display.centerSpace(gradient.GetGradientText(text));
+        }
+
+        public void printCenterRed(string text)
+        {
+            Display display = new Display();
+            Gradient gradient = new Gradient(Color.Orange1, Color.Red1);
+            display.centerSpace(gradient.GetGradientText(text));
+        }
+
+        public void printCenterYellow(string text)
+        {
+            Display display = new Display();
+            Gradient gradient = new Gradient(Color.LightYellow3, Color.Yellow1);
+            display.centerSpace(gradient.GetGradientText(text));
+        }
+
+        public void print(string text)
+        {
+            Gradient gradient = new Gradient(Color.Yellow, Color.Orange3);
+            AnsiConsole.Markup(gradient.GetGradientText(text));
+        }
+
+    }
+}

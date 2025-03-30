84d4f51bf9c3aeb7d9bca2f339cfead718454651
John Paul <johnpaulrayco1@gmail.com>
2025-03-31 03:45:32 +0800

    last push = !vinceBiot ? yes : no;

--- SNAKEANDLADDER-MIDTERM/Program.cs
+++ SNAKEANDLADDER-MIDTERM/Program.cs
@@ -8,6 +8,9 @@ internal class Program
 
     private static void Main(string[] args)
     {
+
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
         MenuOption menuOption = new MenuOption();
         Game game = new Game(menuOption);
         Menu menu = new Menu(menuOption, game);
